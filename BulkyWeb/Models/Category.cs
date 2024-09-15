cf47ffe0698eeebd60c371beab48f9568ec49131
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-09-15 15:40:47 +0300

    Create category table and update the database

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+		[DisplayName("Display Order")]
+        [Range(1, 100)]
+		public int DisplayOrder { get; set; }
+    }
+}
