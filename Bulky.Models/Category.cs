145525dbecf948c90bf210668cb333bc112660ad
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-09-18 15:16:16 +0300

    Move ErrorViewModel class into view models folder in the presentation layer.

--- Bulky.Models/Category.cs
+++ Bulky.Models/Category.cs
@@ -7,14 +7,8 @@ namespace Bulky.Models
     {
         public int Id { get; set; }
 
-        [Required]
-        [DisplayName("Category Name")]
-        [MaxLength(50)]
         public string Name { get; set; } = null!;
 
-        [Required]
-		[DisplayName("Display Order")]
-        [Range(1, 100)]
 		public int DisplayOrder { get; set; }
     }
 }
