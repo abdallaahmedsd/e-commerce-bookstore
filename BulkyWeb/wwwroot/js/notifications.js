214337162268929bb2de07cdcae410b4316b3bad
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-09-18 11:38:49 +0300

    Move notifications into a separate file

---
+++ BulkyWeb/wwwroot/js/notifications.js
@@ -0,0 +1,23 @@
+﻿import '../lib/toastr.js/toastr.min.js';
+
+$(function () {
+    var successMessage = $('#toastr-success-message').data('message');
+    if (successMessage) {
+        toastr.success(successMessage);
+    }
+
+    var errorMessage = $('#toastr-error-message').data('message');
+    if (errorMessage) {
+        toastr.error(errorMessage);
+    }
+
+    var warningMessage = $('#toastr-warning-message').data('message');
+    if (warningMessage) {
+        toastr.warning(warningMessage);
+    }
+
+    var infoMessage = $('#toastr-info-message').data('message');
+    if (infoMessage) {
+        toastr.info(infoMessage);
+    }
+});
