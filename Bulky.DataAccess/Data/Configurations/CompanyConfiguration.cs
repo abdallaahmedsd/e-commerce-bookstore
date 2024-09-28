bf8797a080c00c0691ee60bf5bb7e0878c8deec0
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-09-28 17:07:35 +0300

    Create Company class and it's configurations

---
+++ Bulky.DataAccess/Data/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+namespace Bulky.DataAccess.Data.Configurations
+{
+	internal class CompanyConfiguration : IEntityTypeConfiguration<TbCompany>
+	{
+		public void Configure(EntityTypeBuilder<TbCompany> builder)
+		{
+			builder.HasKey(x => x.Id);
+
+			builder.Property(x => x.Name)
+				.HasMaxLength(150)
+				.IsRequired();
+
+			// Configuring AddressInfo as an owned entity
+			builder.OwnsOne(x => x.AddressInfo, address =>
+			{
+				address.Property(a => a.City)
+					.HasMaxLength(50)
+					.IsRequired(false);
+
+				address.Property(a => a.State)
+					.HasMaxLength(100)
+					.IsRequired(false);
+
+				address.Property(a => a.StreetAddress)
+					.HasMaxLength(150)
+					.IsRequired(false);
+
+				address.Property(a => a.PostalCode)
+					.HasMaxLength(20)
+					.IsRequired(false);
+			});
+
+			builder.ToTable("Companies");
+		}
+	}
+}
---
+++ Bulky.Models/TbCompany.cs
@@ -0,0 +1,16 @@
+﻿using Bulky.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models
+{
+	public class TbCompany
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = null!;
+		public AddressInfo AddressInfo { get; set; } = null!;
+	}
+}
