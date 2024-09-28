0ccaba89ab88182e191a8bd7175b2510c184f9ba
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-09-28 17:21:07 +0300

    Create company controller

---
+++ BulkyWeb/Areas/Company/Controllers/CompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Company.Controllers
+{
+	public class CompanyController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
