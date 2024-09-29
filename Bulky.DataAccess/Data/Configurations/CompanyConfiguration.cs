64ade94e96e736156de7e4f8e07b425f083a3960
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-09-29 12:23:45 +0300

    Seed companies table

--- Bulky.DataAccess/Data/Configurations/CompanyConfiguration.cs
+++ Bulky.DataAccess/Data/Configurations/CompanyConfiguration.cs
@@ -1,43 +1,76 @@
 ﻿using Bulky.Models;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore;
-namespace Bulky.DataAccess.Data.Configurations
+
+internal class CompanyConfiguration : IEntityTypeConfiguration<TbCompany>
 {
-	internal class CompanyConfiguration : IEntityTypeConfiguration<TbCompany>
+	public void Configure(EntityTypeBuilder<TbCompany> builder)
 	{
-		public void Configure(EntityTypeBuilder<TbCompany> builder)
+		builder.HasKey(x => x.Id);
+
+		builder.Property(x => x.Name)
+			.HasMaxLength(150)
+			.IsRequired();
+
+		// Configuring AddressInfo as an owned entity
+		builder.OwnsOne(x => x.AddressInfo, contact =>
 		{
-			builder.HasKey(x => x.Id);
+			contact.Property(x => x.City)
+				.HasMaxLength(50)
+				.IsRequired(false)
+				.HasColumnName("City");
 
-			builder.Property(x => x.Name)
+			contact.Property(x => x.State)
+				.HasMaxLength(100)
+				.IsRequired(false)
+				.HasColumnName("State");
+
+			contact.Property(x => x.StreetAddress)
 				.HasMaxLength(150)
-				.IsRequired();
+				.IsRequired(false)
+				.HasColumnName("StreetAddress");
 
-			// Configuring AddressInfo as an owned entity
-			builder.OwnsOne(x => x.AddressInfo, contact =>
+			contact.Property(x => x.PostalCode)
+				.HasMaxLength(20)
+				.IsRequired(false)
+				.HasColumnName("PostalCode");
+		});
+
+		builder.ToTable("Companies");
+
+		// Seed data for the TbCompany and AddressInfo
+		builder.HasData(
+			new TbCompany { Id = 1, Name = "Tech Innovations" },
+			new TbCompany { Id = 2, Name = "Global Solutions" },
+			new TbCompany { Id = 3, Name = "Future Enterprises" }
+		);
+
+		// Seed data for AddressInfo as part of TbCompany (using the key)
+		builder.OwnsOne(x => x.AddressInfo).HasData(
+			new
 			{
-				contact.Property(x => x.City)
-					.HasMaxLength(50)
-					.IsRequired(false)
-					.HasColumnName("City");
-
-				contact.Property(x => x.State)
-					.HasMaxLength(100)
-					.IsRequired(false)
-					.HasColumnName("State");
-
-				contact.Property(x => x.StreetAddress)
-					.HasMaxLength(150)
-					.IsRequired(false)
-					.HasColumnName("StreetAddress");
-
-				contact.Property(x => x.PostalCode)
-					.HasMaxLength(20)
-					.IsRequired(false)
-					.HasColumnName("PostalCode");
-			});
-
-			builder.ToTable("Companies");
-		}
+				TbCompanyId = 1,
+				City = "Doha",
+				State = "Qatar",
+				StreetAddress = "123 Tech Street",
+				PostalCode = "10001"
+			},
+			new
+			{
+				TbCompanyId = 2,
+				City = "New York",
+				State = "NY",
+				StreetAddress = "456 Global Ave",
+				PostalCode = "10002"
+			},
+			new
+			{
+				TbCompanyId = 3,
+				City = "Berlin",
+				State = "Berlin",
+				StreetAddress = "789 Future Blvd",
+				PostalCode = "10003"
+			}
+		);
 	}
 }
---
+++ Bulky.DataAccess/Migrations/20240928165845_SeedCompaniesTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCompaniesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Name", "City", "PostalCode", "State", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 1, "Tech Innovations", "Doha", "10001", "Qatar", "123 Tech Street" },
+                    { 2, "Global Solutions", "New York", "10002", "NY", "456 Global Ave" },
+                    { 3, "Future Enterprises", "Berlin", "10003", "Berlin", "789 Future Blvd" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
