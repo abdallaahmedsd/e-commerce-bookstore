b88407f23bbbc3b93de9ea05a4ae4b23868ed9e5
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-09-30 12:50:27 +0300

    Create shopping cart table

---
+++ Bulky.DataAccess/Data/Configurations/ShoppingCartConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bulky.DataAccess.Data.Configurations
+{
+	internal class ShoppingCartConfiguration : IEntityTypeConfiguration<TbShoppingCart>
+	{
+		public void Configure(EntityTypeBuilder<TbShoppingCart> builder)
+		{
+			builder.HasKey(x => x.Id);
+
+			builder.HasOne(x => x.Book)
+				.WithMany()
+				.HasForeignKey(x => x.BookId)
+				.IsRequired();
+
+			builder.HasOne(x => x.User)
+				.WithMany()
+				.HasForeignKey(x => x.UserId)
+				.IsRequired();
+
+			builder.ToTable("ShoppingCarts");
+		}
+	}
+}
---
+++ Bulky.Models/TbShoppingCart.cs
@@ -0,0 +1,17 @@
+﻿using Bulky.Models.Identity;
+
+namespace Bulky.Models
+{
+	public class TbShoppingCart
+	{
+        public int Id { get; set; }
+
+        public int BookId { get; set; }
+        public TbBook Book { get; set; } = null!;
+
+        public int Quantity { get; set; }
+
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
