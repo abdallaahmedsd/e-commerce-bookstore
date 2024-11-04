d9a72302320e137a4d40d2a50f71892f77b4d333
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-11-04 19:20:43 +0300

    Delete unnecessary usings

--- Bulky.Models/ViewModels/Admin/UserViewModel.cs
+++ Bulky.Models/ViewModels/Admin/UserViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bulky.Models.ViewModels.Admin
+﻿namespace Bulky.Models.ViewModels.Admin
 {
 	public class UserListViewModel
 	{
