b27a2b20e465c0616139a60bf6f22cd4cb6bf866
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-11-04 19:18:30 +0300

    create a page to show all users

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels.Admin;
+using Bulky.Utility;
+using BulkyWeb.Mappers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+	[Area(SD.Role_Admin)]
+	[Authorize(Roles = SD.Role_Admin)]
+	public class UserController : Controller
+	{
+		private readonly IReadOnlyRepository<UserListViewModel> _readOnlyRepository;
+		private readonly AppDbContext _dbContext;
+
+		public UserController(IReadOnlyRepository<UserListViewModel> readOnlyRepository, AppDbContext dbContext)
+		{
+			_readOnlyRepository = readOnlyRepository;
+			_dbContext = dbContext;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API Calls
+		[HttpGet]
+		public async Task<IActionResult> GetAll()
+		{
+			try
+			{
+				var lstUsers = await _readOnlyRepository.GetAllAsync();
+
+				return Ok(new { success = true, data = lstUsers });
+			}
+			catch (Exception ex)
+			{
+				// Log the exception details (optional)
+				return StatusCode(500, new { success = false, message = "An error occurred while retrieving users." });
+			}
+		}
+		#endregion
+	}
+}
---
+++ BulkyWeb/wwwroot/js/users.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    const dataTable = $('#tblTable').DataTable({
+        ajax: {
+            url: '/admin/user/getAll'
+        },
+        columns: [
+            { data: 'name', 'width': '20%' },
+            { data: 'email', 'width': '20%' },
+            { data: 'phone', 'width': '20%' },
+            { data: 'company', 'width': '15%' },
+            { data: 'role', 'width': '15%', className: 'text-start' },
+            {
+                data: 'id',
+                render: function (data) {
+                    return `
+                        <div class="btn-group" role="group">
+                            <a href="/admin/company/edit/${data}" class="btn btn-warning mx-2">
+                                <i class="bi bi-pencil-square"></i>
+                            </a>
+                        </div>`;
+                },
+                'width': '10%',
+                orderable: false
+            }
+        ],
+        autoWidth: false // Ensures manual column widths are applied properly
+    });
+}
+
