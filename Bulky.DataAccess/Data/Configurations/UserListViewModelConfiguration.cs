40e2c0a937fea6e67c07bac87d0136d0a6ef5094
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-11-04 19:17:44 +0300

    Create user list view and add to database

---
+++ Bulky.DataAccess/Data/Configurations/UserListViewModelConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Bulky.Models.ViewModels.Admin;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bulky.DataAccess.Data.Configurations
+{
+    internal class UserListViewModelConfiguration : IEntityTypeConfiguration<UserListViewModel>
+    {
+        public void Configure(EntityTypeBuilder<UserListViewModel> builder)
+        {
+            builder.HasNoKey()
+                .ToView("UserList_View");
+        }
+    }
+}
---
+++ Bulky.DataAccess/Migrations/20241104160419_CreateUserListView.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateUserListView : Migration
+    {
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.Sql(@"
+                CREATE VIEW UserList_View
+                AS
+                SELECT 
+                    AspNetUsers.Name, 
+                    AspNetUsers.Email, 
+                    AspNetUsers.PhoneNumber AS Phone, 
+                    Companies.Name AS Company, 
+                    AspNetRoles.Name AS Role
+                FROM 
+                    AspNetUserRoles
+                INNER JOIN 
+                    AspNetUsers ON AspNetUserRoles.UserId = AspNetUsers.Id
+                LEFT JOIN 
+                    AspNetRoles ON AspNetUserRoles.RoleId = AspNetRoles.Id
+                LEFT JOIN 
+                    Companies ON AspNetUsers.CompanyId = Companies.Id;
+            ");
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.Sql("DROP VIEW UserList_View;");
+		}
+	}
+}
