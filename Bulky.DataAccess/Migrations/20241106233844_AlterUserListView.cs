d3a8ae217af985f982bfe92669c5ce5172d00445
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-11-07 03:31:21 +0300

    Alter user list view

---
+++ Bulky.DataAccess/Migrations/20241106233844_AlterUserListView.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bulky.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterUserListView : Migration
+    {
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.Sql(@"
+                ALTER VIEW [dbo].[UserList_View]
+                AS
+                SELECT
+					u.Id,
+                    u.Name, 
+                    u.Email, 
+                    u.PhoneNumber AS Phone, 
+                    c.Name AS Company, 
+                    r.Name AS Role,
+					CASE  
+						WHEN u.LockoutEnd > SYSDATETIMEOFFSET() THEN CAST(1 AS BIT)
+						else CAST(0 AS BIT)
+					END AS IsLocked
+                FROM 
+                    AspNetUserRoles ur
+                INNER JOIN 
+                    AspNetUsers u ON ur.UserId = u.Id
+                LEFT JOIN 
+                    AspNetRoles r ON ur.RoleId = r.Id
+                LEFT JOIN 
+                    Companies c ON u.CompanyId = c.Id;
+            ");
+		}
+
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.Sql(@"
+                ALTER VIEW UserList_View
+                AS
+                SELECT 
+                    AspNetUsers.Name, 
+                    AspNetUsers.Email, 
+                    AspNetUsers.PhoneNumber AS Phone, 
+                    Companies.Name AS Company, 
+                    AspNetRoles.Name AS Role
+                FROM 
+                    AspNetUserRoles
+                INNER JOIN 
+                    AspNetUsers ON AspNetUserRoles.UserId = AspNetUsers.Id
+                LEFT JOIN 
+                    AspNetRoles ON AspNetUserRoles.RoleId = AspNetRoles.Id
+                LEFT JOIN 
+                    Companies ON AspNetUsers.CompanyId = Companies.Id;
+            ");
+		}
+	}
+}
--- Bulky.Models/ViewModels/Admin/UserViewModel.cs
+++ Bulky.Models/ViewModels/Admin/UserViewModel.cs
@@ -2,10 +2,12 @@
 {
 	public class UserListViewModel
 	{
+		public int Id { get; set; }
         public string Name { get; set; }
 		public string Email { get; set; } 
 		public string? Phone { get; set; }
 		public string? Company { get; set; }
 		public string Role { get; set; }
+		public bool IsLocked { get; set; }
     }
 }
